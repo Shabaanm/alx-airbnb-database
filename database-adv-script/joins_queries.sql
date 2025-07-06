fc7501472cc6dc348f603b37681a5e07726590d9
Shabaanm <shabaanmoosa@gmail.com>
2025-07-06 19:22:27 +0200

    joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -23,6 +23,8 @@ SELECT
 FROM 
     properties
 LEFT JOIN 
-    reviews ON properties.id = reviews.property_id;
+    reviews ON properties.id = reviews.property_id
+WHERE 
+    reviews.id IS NULL;
 ........................................................
 
