79ec257a24d81a1b09e75355b10c7de57416b742
Shabaanm <shabaanmoosa@gmail.com>
2025-07-06 19:15:50 +0200

    joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -10,8 +10,9 @@ FROM
     bookings
 INNER JOIN 
     users ON bookings.user_id = users.id;
-
-## SELECT 
+-- Retrieve all properties and their reviews,
+-- including properties that have no reviews
+SELECT 
     properties.id AS property_id,
     properties.name AS property_name,
     reviews.id AS review_id,
