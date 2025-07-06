d4cddbd3909a6c3dd5ceb277ffeb18e0c0e5736e
Shabaanm <shabaanmoosa@gmail.com>
2025-07-06 19:59:13 +0200

    partitioning.sql

--- database-adv-script/partitioning.sql
+++ database-adv-script/partitioning.sql
@@ -1,1 +1,10 @@
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    property_id INT NOT NULL,
+    payment_id INT,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    -- other columns
+) PARTITION BY RANGE (start_date);
 
