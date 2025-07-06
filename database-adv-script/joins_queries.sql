9d7c09053a31158cf17083c918a34533b4dc904d
Shabaanm <shabaanmoosa@gmail.com>
2025-07-06 19:28:03 +0200

    joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -26,6 +26,20 @@ FROM
 LEFT JOIN 
     reviews ON properties.id = reviews.property_id;
 
+........................................................
 
+## Full Outer, 
+SELECT 
+    users.id AS user_id,
+    users.name AS user_name,
+    users.email AS user_email,
+    bookings.id AS booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date
+FROM 
+    users
+FULL OUTER JOIN 
+    bookings ON users.id = bookings.user_id;
 ........................................................
 
